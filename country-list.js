0298504ef5a44b84c7135d87043e0f77b6aba13c
muratisik167-prog <muratisik167@gmail.com>
2025-12-11 21:18:27 +0300

    Create country-list.js

---
+++ country-list.js
@@ -0,0 +1,14 @@
+let country_list = {
+    "USD": "US",
+    "EUR": "FR", // Euro bölgesi için temsili
+    "TRY": "TR",
+    "GBP": "GB",
+    "JPY": "JP",
+    "CAD": "CA",
+    "AUD": "AU",
+    "RUB": "RU",
+    "CNY": "CN",
+    "CHF": "CH",
+    "SAR": "SA",
+    "AZN": "AZ"
+}
